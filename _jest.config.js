793968fb9534bb0edcd341bae1cc84f27553636c
kerolos <kerolos.sss@gmail.com>
2023-03-08 01:24:24 +0200

    chore: cleanup

--- _jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // testEnvironment: "node",
-  // roots: ["./src"],
-  // // preset: 'ts-jest',
-  // coveragePathIgnorePatterns: ["/node_modules/"],
-  // testRegex: "((\\.|/*.)(spec))\\.js?$",
-  // transform: {}
-};
